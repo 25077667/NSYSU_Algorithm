89bfa728daac0eb67663edadbe521c181a5861ec
25077667 <zxc25077667@gmail.com>
2020-11-20 21:56:02 +0800

    FIXME, wrong length and path

--- hw2/main.cpp
+++ hw2/main.cpp
@@ -1,86 +1,83 @@
 #include <algorithm>
 #include <cmath>
-#include <future>
 #include <iostream>
 #include <limits>
-#include <thread>
+#include <map>
 #include <tuple>
 #include <vector>
 
 using namespace std;
 
-inline double getDistance(tuple<int, int, int> a, tuple<int, int, int> b)
+
+// result format: (length, path)
+// @length: A double of path length
+// @path: A vector of cities, the order of vector is the order of minimal cities
+// path
+pair<double, vector<tuple<string, int, int>>> result;
+// DP table
+map<int, double> traversal;
+
+inline double getDistance(tuple<string, int, int> a, tuple<string, int, int> b)
 {
     return sqrt(pow(get<1>(a) - get<1>(b), 2) + pow(get<2>(a) - get<2>(b), 2));
 }
 
-double getPathLen(vector<tuple<int, int, int>> cities)
+// Update the global variable result
+double dp(const vector<tuple<string, int, int>> &cities,
+          int current,
+          int recorded)
 {
-    double total = 0;
-    tuple<int, int, int> prev;
-    for (auto i : cities) {
-        if (i != *cities.begin())  // Not the first time enter this loop
-            total += getDistance(prev, i);
-        prev = i;
-    }
-    // Back to the start point
-    total += getDistance(prev, *cities.begin());
-    return total;
-}
-
-auto dp(vector<tuple<int, int, int>> cities)
-{
-    double minPath = numeric_limits<double>::max();
-    vector<tuple<int, int, int>> cacheStatus;
-    do {
-        auto pathLen = getPathLen(cities);
-        if (minPath > pathLen) {
-            cacheStatus = cities;
-            minPath = pathLen;
+    auto isRecorded = traversal.find(recorded);
+    if (isRecorded == traversal.end()) {  // not found
+        int mask = 1 << current;
+        int minIndex = -1;
+        traversal[recorded | mask] = numeric_limits<double>::max();
+        for (int i = 0; i < cities.size(); i++) {
+            if (i != current && recorded & (1 << i)) {
+                auto fromDP = dp(cities, i, recorded & (~mask)) +
+                              getDistance(cities.at(i), cities.at(current));
+                if (fromDP < traversal[recorded | mask]) {
+                    minIndex = i;
+                    traversal[recorded | mask] = fromDP;
+                }
+            }
         }
-    } while (next_permutation(cities.begin() + 2, cities.end()));
-
-    return make_pair(minPath, cacheStatus);
+        result.second.push_back(cities.at(minIndex));
+        return traversal[recorded | current];
+    } else {
+        return isRecorded->second;
+    }
 }
 
-void output(const vector<pair<double, vector<tuple<int, int, int>>>> &result){
-    for (auto i : result.at(0).second)
+void output()
+{
+    for (auto i : result.second)
         cout << get<0>(i) << " " << get<1>(i) << " " << get<2>(i) << endl;
-    cout << get<0>(result.at(0).second.at(0)) << " "
-         << get<1>(result.at(0).second.at(0)) << " "
-         << get<2>(result.at(0).second.at(0)) << endl;
+    cout << get<0>(result.second.at(0)) << " " << get<1>(result.second.at(0))
+         << " " << get<2>(result.second.at(0)) << endl;
 
-    cout << endl << "Minimal length of path: " << result.at(0).first << endl;
+    cout << endl << "Minimal length of path: " << result.first << endl;
+}
+
+void init(int numCities)
+{
+    for (int i = 0; i < numCities; i++)
+        traversal[1 << i] = 0;
 }
 
 // Use pipeline to put in the stdin, plz
 int main()
 {
-    int num, x, y;
-    vector<tuple<int, int, int>> cities;
-    vector<pair<double, vector<tuple<int, int, int>>>> result;
-    vector<future<pair<double, vector<tuple<int, int, int>>>>> threadPool;
-    while (cin >> num >> x >> y)
-        cities.push_back(make_tuple(num, x, y));
+    int x, y;
+    string name;
+    vector<tuple<string, int, int>> cities;
+    while (cin >> name >> x >> y)
+        cities.push_back(make_tuple(name, x, y));
 
-    threadPool.resize(cities.size() - 1);
-    sort(cities.begin(), cities.end());
-    for (auto i = 1; i < cities.size(); i++) {
-        iter_swap(cities.begin() + 1, cities.begin() + i);
-        threadPool[i - 1] = async(bruteForce, cities);
-        iter_swap(cities.begin() + 1, cities.begin() + i);
-    }
+    init(cities.size());
 
-    for (auto iter = threadPool.begin(); iter != threadPool.end(); iter++)
-        result.push_back((*iter).get());
+    result.first = dp(cities, 0, (1 << cities.size()) - 1);
 
-    sort(result.begin(), result.end());
-    for (auto i : result.at(0).second)
-        cout << get<0>(i) << " " << get<1>(i) << " " << get<2>(i) << endl;
-    cout << get<0>(result.at(0).second.at(0)) << " "
-         << get<1>(result.at(0).second.at(0)) << " "
-         << get<2>(result.at(0).second.at(0)) << endl;
-
-    cout << endl << "Minimal length of path: " << result.at(0).first << endl;
+    output();
     return 0;
 }
