397cff4442ddfb0411bfa15df75eeee6b6b28296
25077667 <zxc25077667@gmail.com>
2020-11-22 16:33:50 +0800

    Change to traverse all city, rather than toggled city

--- hw2/main.cpp
+++ hw2/main.cpp
@@ -11,9 +11,10 @@
 #define MAX_CITIES 16
 
 using namespace std;
+typedef tuple<string, int, int> City;
 
 
-inline double getDistance(tuple<string, int, int> a, tuple<string, int, int> b)
+inline double getDistance(City a, City b)
 {
     return sqrt(pow(get<1>(a) - get<1>(b), 2) + pow(get<2>(a) - get<2>(b), 2));
 }
@@ -28,43 +29,42 @@ inline double getDistance(tuple<string, int, int> a, tuple<string, int, int> b)
 // @cities: A const vector to get cities info from user.
 // @current: The current index of the city.
 // @recordMask: A mask to recording "viewed" cities.
-pair<double, vector<tuple<string, int, int>>> dp(
-    map<int, array<double, MAX_CITIES>> &traversal,
-    vector<tuple<string, int, int>> records,
-    const vector<tuple<string, int, int>> &cities,
-    int current,
-    int recordMask)
+pair<double, vector<City>> dp(map<int, array<double, MAX_CITIES>> &traversal,
+                              vector<City> records,
+                              const vector<City> &cities,
+                              int current,
+                              int recordMask)
 {
     auto isRecorded = traversal.find(recordMask);
     if (isRecorded != traversal.end() &&
         isRecorded->second.at(current) != 0) {  // found record
         return make_pair(isRecorded->second.at(current), records);
     } else {
-        double minPathLen = numeric_limits<double>::max();
-        vector<tuple<string, int, int>> minPath;
-        while (recordMask) {
-            // The canonical algorithm is a loop counting zeros starting at the
-            // LSB until a 1-bit is encountered
-            auto ctz = __builtin_ctz(recordMask);
-            recordMask &= ~(1 << ctz);
+        double minPathLen = numeric_limits<double>::infinity();
+        int minMask = 0;
+        vector<City> minPath;
+        for (int i = 0;
+             i < cities.size() && ((1 << i) & recordMask) && i != current;
+             i++) {
             auto [passPathLen, passCities] =
-                dp(traversal, records, cities, ctz, recordMask);
+                dp(traversal, records, cities, i, recordMask ^ (1 << current));
             if (passPathLen < minPathLen) {
                 minPathLen = passPathLen;
                 minPath = passCities;
+                minMask = recordMask ^ (1 << current);
             }
         }
 
         auto mask = 1 << current;
         auto origBack = (minPath.size() ? minPath.back() : cities.at(current));
         minPath.push_back(cities.at(current));
-        traversal[recordMask | mask].at(current) =
-            getDistance(origBack, cities.at(current));
-        return make_pair(traversal[recordMask | mask].at(current), minPath);
+        traversal[minMask | mask].at(current) =
+            getDistance(origBack, cities.at(current)) + minPathLen;
+        return make_pair(traversal[minMask | mask].at(current), minPath);
     }
 }
 
-void output(const pair<double, vector<tuple<string, int, int>>> result)
+void output(const pair<double, vector<City>> result)
 {
     for (auto i : result.second)
         cout << get<0>(i) << " " << get<1>(i) << " " << get<2>(i) << endl;
@@ -77,7 +77,7 @@ void output(const pair<double, vector<tuple<string, int, int>>> result)
 // Suppose starts at the first city
 // Initialize `traversal` with cityes' distance
 void init(map<int, array<double, MAX_CITIES>> &traversal,
-          const vector<tuple<string, int, int>> cities)
+          const vector<City> cities)
 {
     for (int i = 0; i < cities.size(); i++) {
         memset(&traversal[1 << i], 0, MAX_CITIES);
@@ -90,7 +90,7 @@ int main()
 {
     int x, y;
     string name;
-    vector<tuple<string, int, int>> cities, records;
+    vector<City> cities, records;
     while (cin >> name >> x >> y)
         cities.push_back(make_tuple(name, x, y));
     sort(cities.begin(), cities.end());
