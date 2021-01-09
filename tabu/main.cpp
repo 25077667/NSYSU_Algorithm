4934b06d0033db4a8f76c27eb1b5927683f1ed4a
25077667 <zxc25077667@gmail.com>
2021-01-09 14:47:33 +0800

    Add prototype for main

---
+++ tabu/main.cpp
@@ -0,0 +1,13 @@
+#include <array>
+#include <iostream>
+#include <tuple>
+#include <vector>
+using namespace std;
+
+typedef tuple<int, int, int> City;
+
+int main()
+{
+    vector<City> cities;
+    return 0;
+}
